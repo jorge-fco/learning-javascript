9015a56ebded35c4397e858ebd91593df212ecad
Jorge Fco <jorge.fco.mend@gmail.com>
2018-10-26 15:47:59 -0500

    Init variables js

--- js/variables.js
+++ js/variables.js
@@ -1,9 +1,22 @@
-//VARS
+//= Vars
 const msg = "Welcome";
 var name = "George";
-var num = 7;
+var lastname;
+var age = 17;
+let total = 0, count = 1;
 
-console.log('V A R I A B L E S ');
-console.log('------------------------');
-console.log(msg+' | '+cad);
-console.log('Num : '+num);
+console.log("V A R I A B L E S\n------------------------");
+console.log(msg+" - "+name);
+console.log("age : "+age);
+while (count <= 10) {
+	total += count;
+	count += 1;
+}
+console.log("Total : "+total);
+console.log("A newline character is written like \"\\n\".");
+console.log(lastname==undefined);
+
+console.log(typeof 4.5);
+// → number
+console.log(typeof "x");
+// → string
